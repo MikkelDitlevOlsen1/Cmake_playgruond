c80f3933c9a0cf5ed7848059bf06d2796de1cadb
MikkelDilevOlsen1 <s204094@dtu.dk>
2024-02-26 12:52:09 +0100

    first comit

---
+++ cmake-3.29.0-rc2-tutorial-source/Step1/CMakeFiles/Totorial.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Totorial.dir/tutorial.cxx.o"
+  "CMakeFiles/Totorial.dir/tutorial.cxx.o.d"
+  "Totorial"
+  "Totorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Totorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
