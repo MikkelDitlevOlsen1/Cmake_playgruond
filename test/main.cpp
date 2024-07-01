cc87193a44e719c86ef4317cee0a5233a0b42873
MikkelDilevOlsen1 <s204094@dtu.dk>
2024-07-01 11:08:54 +0200

    change

--- test/main.cpp
+++ test/main.cpp
@@ -70,7 +70,7 @@ void startPWMSignal() {
 
 int main() {
     // Initialize pigpio
-    int sleptime=10000000;
+    int sleptime=1000000;
     std::cout << "Initializing pigpio" << std::endl;
     if (gpioInitialise() < 0) {
         std::cerr << "pigpio initialization failed!" << std::endl;
